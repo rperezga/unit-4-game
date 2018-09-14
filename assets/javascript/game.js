b1e615cf99a3bacb8e074c227a7eb8fc6fcfd52c
Roger Perez <rperez@OneHealth.com>
2018-09-14 09:53:49 -0400

    Working on choose the characters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,7 @@
 $(function(){
 
-    var character = ['apu', 'bart', 'comicbookguy', 'homer', 'itchy', 'krusty', 'lisa', 'maggie', 'marge', 'mr-burns', 'ned-flanders', 'ralph-wiggum', 'smithers']
+    var character = ['apu', 'bart', 'comicbookguy', 'homer', 'itchy', 'krusty', 'lisa', 'maggie', 'marge', 'mr-burns', 'ned-flanders', 'ralph-wiggum', 'smithers'];
+    var randomCharacter = [];
 
     $("#start").on("click", function(){
         var x = document.getElementById("myWelcome");
@@ -9,6 +10,26 @@ $(function(){
         if (x.style.display != "none") {
             x.style.display = "none";
             y.style.display = "block";
+        }
+
+        while(randomCharacter.length < 4){
+            var randomN = Math.floor(Math.random() * 13);
+            randomCharacter.push(character[randomN]);            
+        }
+
+        for(var i =0; i < randomCharacter.length; i++){
+            console.log(randomCharacter[i])
+            if(randomCharacter[i] === 'marge'){
+                var newDiv = $("<div class='col'>");
+                newDiv.html('<object data="assets/simpsons/' + randomCharacter[i] + '.html" />');
+                $("#show-character").append(newDiv);
+            }else{
+                var newDiv = $("<div class='col'>").css({"margin-top": "60px"});
+                newDiv.html('<object data="assets/simpsons/' + randomCharacter[i] + '.html" />');
+                $("#show-character").append(newDiv);
+            }
+            
+            
         }
     });
 
@@ -34,14 +55,6 @@ $(function(){
             document.getElementById("start").disabled = true;
         }
     }
-
-
-    $("#character").on("click", function(){
-        var randomN = Math.floor(Math.random() * 13);
-
-        $("#show-character").html('<object data="assets/simpsons/' + character[randomN] + '.html" />');
-    });
-
 
 
     
