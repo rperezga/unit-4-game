772543783413183f51589330c15b89c91c4f9722
Roger Perez <rperezga@gmail.com>
2018-09-16 09:17:54 -0400

    Working on the object

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,49 @@
 $(function(){
 
-    var character = ['apu', 'bart', 'comicbookguy', 'homer', 'itchy', 'krusty', 'lisa', 'maggie', 'marge', 'mr-burns', 'ned-flanders', 'ralph-wiggum', 'smithers'];
+    
     var randomCharacter = [];
+    var yourName = '';
 
-    $("#start").on("click", function(){
+    var character = {
+        characters = ['apu', 'bart', 'comicbookguy', 'homer', 'itchy', 'krusty', 'lisa', 'maggie', 'marge', 'mr-burns', 'ned-flanders', 'ralph-wiggum', 'smithers'],
+        
+        name: "",
+        healthPoints: 0,
+        attackPower: 0,
+        counterAttackPower: 0,
+
+        get start(){
+            while(randomCharacter.length < 4){
+                var randomC = Math.floor(Math.random() * 13);
+                character.name = characters[randomC];
+                character.healthPoints = Math.floor(Math.random() * 100) + 100;
+                
+
+
+
+            }
+            
+
+
+            if(randomCharacter.indexOf(character[randomC]) < 0){
+                randomCharacter.push(character[randomN]); 
+            }
+
+
+            return (this.character);
+        }
+    }
+
+
+});
+
+
+/*
+
+
+    // To start the game and ramdomly generate the characters for the game.
+    $("#start").on("click", function(){        
+
         var x = document.getElementById("myWelcome");
         var y = document.getElementById("myGame");
 
@@ -22,19 +62,22 @@ $(function(){
         for(var i =0; i < randomCharacter.length; i++){
             console.log(randomCharacter[i])
             if(randomCharacter[i] === 'marge'){
-                var newDiv = $("<div class='col'>");
+                var newDiv = $("<div class='col' id='character'>");
                 newDiv.html('<object data="assets/simpsons/' + randomCharacter[i] + '.html" />');
                 $("#show-character").append(newDiv);
             }else{
-                var newDiv = $("<div class='col'>").css({"margin-top": "60px"});
+                var newDiv = $("<div class='col' id='character'>").css({"margin-top": "60px"});
                 newDiv.html('<object data="assets/simpsons/' + randomCharacter[i] + '.html" />');
                 $("#show-character").append(newDiv);
             }
-            
-            
         }
+
+        yourName = $("#yourName").val();
+        $("#hello").text("Hello " + yourName + ". Please choose your Character!");
     });
 
+
+    // To exit the game and reset all variables.
     $("#finish").on("click", function(){
         var x = document.getElementById("myWelcome");
         var y = document.getElementById("myGame");
@@ -46,23 +89,34 @@ $(function(){
 
         document.getElementById('yourName').value = '';
         document.getElementById("start").disabled = true;
+
+        randomCharacter = [];
+        $("#show-character").text('');        
     });
 
 
+    // To verify if I have type something on yourName input field.
     document.onkeyup = function (event) {
-        name = document.getElementById('yourName').value;
-        if (name.length > 0 && name != '') {
+        yourName = document.getElementById('yourName').value;
+        if (yourName.length > 0 && yourName != '') {
             document.getElementById("start").disabled = false;
         } else {
             document.getElementById("start").disabled = true;
         }
     }
 
+    $("#character").click(function(){
+        alert('Di click');
+    });
+
 
     
 
 
+    
+*/
 
 
-});
+
+
 
